cef3e819756b42148ecbc4acfed6d050cd465ee1
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-03-24 08:14:15 +0530

    added

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -1,9 +1,26 @@
 let addBtn = document.querySelector('.add-btn');
 
-addBtn.addEventListener('click',function(){
+let modelCont = document.querySelector('.model-cont');
+
+let addFlag = false;
+
+addBtn.addEventListener('click', function (e) {
 
     //Display a modal
+    //addFlag - true -> Display the model
+    //addFlag - false -> Hide the model
 
+    addFlag = !addFlag;
+
+    if (addFlag) {
+
+        modelCont.style.display = 'flex';
+    }
+
+    else {
+
+        modelCont.style.display = 'none';
+    }
 
 
     //Add a cards
